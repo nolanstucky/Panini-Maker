1eb0d021e73fcc72810f74cd1804d99856e67bef
Nolan Stucky <nolan@stucky.net>
2021-02-16 00:04:10 -0800

    added delete route

--- controllers/paniniController.js
+++ controllers/paniniController.js
@@ -26,7 +26,6 @@ router.put("/api/panini/:id", function(req, res) {
       eaten: req.body.eaten
     }, condition, function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
@@ -46,6 +45,16 @@ router.post("/api/panini", function(req, res) {
     });
   });
 
+router.delete("/api/burgers/:id", function(req,res){
+  var condition = `id = ${req.params.id}`;
 
+  paninis.delete(condition, function(result){
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  })
+})
 // Export routes for server.js to use.
 module.exports = router;
